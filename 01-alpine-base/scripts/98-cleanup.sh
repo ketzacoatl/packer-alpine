56d33cc111ce923c68f13725d7299da49de2f4e6
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2019-05-04 04:10:52 +0200

    basic install, box file around 37 MB

--- 01-alpine-base/scripts/98-cleanup.sh
+++ 01-alpine-base/scripts/98-cleanup.sh
@@ -1,5 +1,13 @@
+#!/bin/sh -eux
+set -eux
+
+apk info --installed salt-minion && apkRESULT="${?}" || apkRESULT="${?}"
+	if [[ $apkRESULT == "0" ]]; then
+		rc-service salt-minion stop
+	fi
+
+
 
 rm -rf /var/cache/apk/*
 rm -rf /etc/ssh/ssh_host_*
-
-rc-service salt-minion stop
+sync
